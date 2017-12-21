e5bfa8c589c2f29d834158d425ec177cc3913d66
Jordan Snow <snowstorm1989@gmail.com>
2017-12-21 15:24:34 -0600

    closure done

--- closures.js
+++ closures.js
@@ -4,7 +4,7 @@
 
 function outer() {
     var name = 'Tyler';
-    return function() {
+    return function () {
       return 'The original name was ' + name;
     }
   }
@@ -15,11 +15,12 @@ function outer() {
   another variable called 'inner'. */
   
   // Code Here
-  
+  var inner = outer()
+
   //Once you do that, invoke inner.
   
   //Code Here
-  
+  inner()
   
   
   
@@ -51,8 +52,14 @@ function outer() {
   */
   
     //Code Here
+  // var callJake = callFriend(Jake)
+
   
+function callJake(number){
+  return callFriend('Jake')(number)
+}
   
+callJake('435-555-9248')
   
   
   
@@ -69,13 +76,20 @@ function outer() {
   properly. */
   
   //Code Here
-  
+  function makeCounter(){
+    var num = 0
+    return function(){
+      return num += 1
+    }
+  }
+
+
   //Uncomment this once you make your function
-  //   var count = makeCounter();
-  //   count(); // 1
-  //   count(); // 2
-  //   count(); // 3
-  //   count(); // 4
+    var count = makeCounter();
+    // count(); // 1
+    // count(); // 2
+    // count(); // 3
+    // count(); // 4
   
   
   
@@ -101,15 +115,17 @@ function outer() {
   */
   
   function counterFactory(value) {
-  
-    // Code here.
-  
-  
     return {
-
+      inc: function(){
+      
+        return value += 1;
+      },
+      dec: function(){
+        
+        return value -= 1;
+      }
     }
   }
-  
   
   counter = counterFactory(10);
   // counter.inc() // 11
@@ -142,10 +158,12 @@ function outer() {
     var welcomeText = 'You\'re doing awesome, keep it up ';
   
     // code message function here.
-  
+  function message(){
+    return `${welcomeText}${firstname} ${lastname}.` ;
+  }
   
     //Uncommment this to return the value of your message function
-    //return message;
+    return message;
   
   }
   
@@ -183,7 +201,7 @@ function outer() {
     // Anything that is being returned is made public and can be invoked from
     // outside our lexical scope
     return {
-      // Code here.
+      publicMethod: () => { return privateMethod()}
     };
   
   })();
@@ -202,9 +220,12 @@ function outer() {
     var secret = 143;
 
     return {
-      // Code here
-    }
-  }
+      addToSecret: (num) => secret += num,
+      takeAwayFromSecret: (num) => secret -= num
+      }
+    return secret
+   }
+  secretNumber.AddToSecret(10)
   
   
   
@@ -229,11 +250,13 @@ function outer() {
    */
   
   function timeOutCounter() {
-    for (var i = 0; i <= 5; i++) {
+    function wrapper(index) {
       setTimeout(function() {
-          console.log(i)
-      }, i * 1000)
-    }
+        console.log(index)
+      }, index * 1000)
+    } 
+    for (var i = 0; i <= 5; i++) {
+    wrapper(i)
   }
-  timeOutCounter();
-  
+}
+timeOutCounter();
